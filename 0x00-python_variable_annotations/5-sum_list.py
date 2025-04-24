a7c3ca31e1bc1e704be20b5dfae82db3026da7d9
Martin George <martingeorge191110@gmail.com>
2025-04-24 21:29:47 +0200

    task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+sum_list function
+"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """sum of all numbers in a List"""
+    return (sum(input_list))
