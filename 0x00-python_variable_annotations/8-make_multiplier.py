c07a459cd71a8064cf7dc20d7d241ce8f7426adb
Martin George <martingeorge191110@gmail.com>
2025-04-24 21:31:57 +0200

    task 8

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,12 @@
+
+#!/usr/bin/env python3
+"""
+make_multiplier function
+"""
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """return tuple of str and square of second number"""
+    return (lambda x: (x * multiplier))
