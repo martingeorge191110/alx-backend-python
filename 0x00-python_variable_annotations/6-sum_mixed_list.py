204ea497ffacbece32942f7b718e9ba8647d67ce
Martin George <martingeorge191110@gmail.com>
2025-04-24 21:30:43 +0200

    task 6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+sum_mixed_list function
+"""
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """sum of all numbers in a List"""
+    return (sum(mxd_lst))
